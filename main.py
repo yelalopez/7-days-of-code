c37520a70270a67a8fa10bed4a9386e1908285f6
yelalopez <yelitzalopezduque@gmail.com>
2024-09-26 10:39:52 -0300

    day 1: set up initial stock data and modified lists

---
+++ main.py
@@ -0,0 +1,10 @@
+stock = ['notebook', 'pencil', 'pen', 'eraser', 'ruler', 'highlighter', 'glue stick', 'paper clip', 'stapler', 'scissors']
+
+stock.append('sticky notes')
+stock.append('colored pencils')
+
+print(f'Estoque inicial: {stock}')
+
+stock.pop(0)
+
+print(f'Estoque atualizado: {stock}')
