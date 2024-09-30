944f88f03b07294c36843e36c3ed9de1d0354e43
yelalopez <yelitzalopezduque@gmail.com>
2024-09-30 10:00:22 -0300

    day 1: Initializing stock and manipulating lists

---
+++ day1.py
@@ -0,0 +1,10 @@
+stock = ['notebook', 'pencil', 'pen', 'eraser', 'ruler', 'highlighter', 'glue stick', 'paper clip', 'stapler', 'scissors']
+
+stock.append('sticky notes')
+stock.append('colored pencils')
+
+print(f'Stock: {stock}')
+
+stock.pop(0)
+
+print(f'Stock updated: {stock}')
